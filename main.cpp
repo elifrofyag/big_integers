bbf1c406369f3b035ab112e4f9f4331000902557
elifrofyag <annguyenvanb3@gmail.com>
2025-04-12 21:16:09 +0700

    main funct

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include "operation.h"
+#include "precedence.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        cerr << "Usage: ./executable_file input_file output_file" << endl;
+        return 1;
+    }
+
+    ifstream inFile(argv[1]);
+    if (!inFile) {
+        cerr << "Cannot open input file: " << argv[1] << endl;
+        return 1;
+    }
+
+    ofstream outFile(argv[2]);
+    if (!outFile) {
+        cerr << "Cannot open output file: " << argv[2] << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(inFile, line)) {
+        if (line.empty()) continue;
+        bool error = false;
+        string result = evaluateExpression(line, error);
+        cout << result << endl;
+        outFile << result << endl;
+    }
+
+    inFile.close();
+    outFile.close();
+    return 0;
+}
+
+
